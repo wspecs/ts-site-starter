8cd1e274c9ac4c3f6d7437f3f33e14792cb66b6f
Wendly Saintil <wspecs@hotmail.com>
2018-05-17 21:23:30 -0700

    export custom typings directly

--- index.ts
+++ index.ts
@@ -2,14 +2,15 @@ import * as restConfig from './lib/rest-config';
 import * as routerConfig from './lib/router-config';
 import {BasicRoutes} from './lib/router';
 import * as serverConfig from './lib/server-config';
-import * as types from './lib/types';
+import {AppRequest, AppResponse} from './lib/types';
 import {handlerDecorator} from './lib/utils';
 
 export {
+  AppRequest,
+  AppResponse,
   BasicRoutes,
   restConfig,
   routerConfig,
   serverConfig,
-  types,
   handlerDecorator
 };
