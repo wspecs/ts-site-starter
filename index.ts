825535f7802aea9507f14d10249d62ef83e7a925
Wendly Saintil <wspecs@hotmail.com>
2018-05-17 21:16:41 -0700

    update export list

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 import * as restConfig from './lib/rest-config';
 import * as routerConfig from './lib/router-config';
-import * as router from './lib/router';
+import {BasicRoutes} from './lib/router';
 import * as serverConfig from './lib/server-config';
 import * as types from './lib/types';
 import * as utils from './lib/utils';
 
 export {
+  BasicRoutes,
   restConfig,
   routerConfig,
-  router,
   serverConfig,
   types,
   utils
